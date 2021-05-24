55504a60d604e0d5acfa906321ebc0b3859eb91e
vitomedved <vito.medved12@gmail.com>
2021-05-24 22:34:44 +0200

    Added Lottie animation

--- app/src/main/java/com/example/lastfmuselessapp/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/example/lastfmuselessapp/ui/onboarding/OnboardingScreen.kt
@@ -7,34 +7,46 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieAnimationSpec
+import com.example.lastfmuselessapp.R
 
 @Composable
 fun OnboardingScreen(
     onFinishOnboardingClicked: () -> Unit
 ) {
+    val lottieAnimationSpec = LottieAnimationSpec.RawRes(R.raw.artist_animation)
+
     Column(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
             .fillMaxHeight()
     ) {
+
+        LottieAnimation(
+            spec = lottieAnimationSpec,
+            modifier = Modifier
+                .padding(12.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
         Button(onClick = { onFinishOnboardingClicked() }) {
             Text(text = "Go home")
         }
+
+
     }
-}
-
-@Composable
-fun TextViewWithShapeBackground() {
-    Text(text = "test")
-
 }
 
 @Preview
 @Composable
 fun Preview() {
-    TextViewWithShapeBackground()
+    OnboardingScreen {
+
+    }
 }
